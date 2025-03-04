6c8f5c6680d934add6bcc20ab5c245efbec3e88c
spacetinker <davidrobertmorea@gmail.com>
2025-03-04 12:39:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+var tutorial = 0;
+function continue_intro() {
+  document.GetElementById("intro1").hidden = true;
+  document.GetElementById("intro2").hidden = true;
+  document.GetElementById("intro3").hidden = true;
+  document.GetElementById("intro4").hidden = true;
+  tutorial = 1;
+}
